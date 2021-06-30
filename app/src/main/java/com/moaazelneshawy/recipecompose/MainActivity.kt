b15e5ba0e88d103e3feca61b9a1a03867aaadd46
MoaazElneshawy <m.elneshawy@gmail.com>
2021-06-30 09:34:56 +0300

    first init

---
+++ app/src/main/java/com/moaazelneshawy/recipecompose/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.moaazelneshawy.recipecompose
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.moaazelneshawy.recipecompose.ui.theme.RecipeComposeTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
